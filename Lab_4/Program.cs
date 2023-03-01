82d5485dcf8f8b098b3dea093009d540f5d554ee
CYKERO4KA <cykero4ka88@gmail.com>
2023-03-01 19:55:29 +0200

    First commit

---
+++ Lab_4/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Bicycle
+    {
+        
+    }
+
+    class Wheel
+    {
+        
+    }
+
+    class Frame
+    {
+        
+    }
+
+    class Helm
+    {
+        
+    }
+
+    class Pedals
+    {
+        
+    }
+}
